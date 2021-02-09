86033a3e8f20403ba64c8a84da7c123e34078093
saikat1095 <mdabuthaersaikat@gmail.com>
2021-02-10 00:15:18 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import NotFound from './components/NotFound';
+
 
 const App = () => {
     return (
-        <div>
-            
-        </div>
+        <Router>
+            <div className="App">
+                <Navbar></Navbar>
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+
+                        <Route exact path="/about" component={About}>
+                            {/* <About /> */}
+                        </Route>
+
+                        <Route exact path="*">
+                            <NotFound />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
     )
 }
 
